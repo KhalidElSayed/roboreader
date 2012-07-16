c6b92ffccc8cfb81cd24a4c7e54a31bfc530121e
Christof Dorner <christof@chdorner.me>
2012-07-16 22:36:43 +0200

    Add initial Android project

---
+++ src/com/chdorner/roboreader/LibraryActivity.java
@@ -0,0 +1,15 @@
+package com.chdorner.roboreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LibraryActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
