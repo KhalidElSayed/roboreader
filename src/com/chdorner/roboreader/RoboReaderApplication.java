2b12b3c0f3bd59c9e9ba84c696b72fc94ec2db64
Christof Dorner <christof@chdorner.me>
2012-07-17 22:04:53 +0200

    Add simple DatabaseHelper and load on app startup

--- src/com/chdorner/roboreader/RoboReaderApplication.java
+++ src/com/chdorner/roboreader/RoboReaderApplication.java
@@ -3,11 +3,17 @@ package com.chdorner.roboreader;
 import android.app.Application;
 import android.util.Log;
 
+import com.chdorner.roboreader.persistence.DatabaseHelper;
+
 public class RoboReaderApplication extends Application {
   protected final String TAG = this.getClass().getName();
 
+  private DatabaseHelper databaseHelper;
+
   public void onCreate() {
     super.onCreate();
+
+    this.databaseHelper = new DatabaseHelper(this);
   }
 }
 
---
+++ src/com/chdorner/roboreader/persistence/DatabaseHelper.java
@@ -0,0 +1,36 @@
+package com.chdorner.roboreader.persistence;
+
+import java.sql.SQLException;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+  protected final String TAG = this.getClass().getName();
+
+  private static final String DATABASE_NAME = "roboreader.db";
+  private static final int DATABASE_VERSION = 1;
+
+  public DatabaseHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
+
+  public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
+    Log.d(TAG, "onCreate");
+    try {
+      TableUtils.createTable(connectionSource, Book.class);
+    } catch(SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+    Log.d(TAG, "onUpgrade");
+  }
+}
+
