6db0defa45a03d9f988a1ae81f26e66f10020781
Christof Dorner <christof@chdorner.me>
2012-07-17 21:37:04 +0200

    Add RoboReaderApplication skeleton

---
+++ src/com/chdorner/roboreader/RoboReaderApplication.java
@@ -0,0 +1,13 @@
+package com.chdorner.roboreader;
+
+import android.app.Application;
+import android.util.Log;
+
+public class RoboReaderApplication extends Application {
+  protected final String TAG = this.getClass().getName();
+
+  public void onCreate() {
+    super.onCreate();
+  }
+}
+
