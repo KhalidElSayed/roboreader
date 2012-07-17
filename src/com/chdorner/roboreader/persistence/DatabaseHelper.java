22631f08580a0f4db2a2a266f5bf0579991d1de3
Christof Dorner <christof@chdorner.me>
2012-07-17 22:18:18 +0200

    Add getBookDao method to DatabaseHelper

--- src/com/chdorner/roboreader/persistence/DatabaseHelper.java
+++ src/com/chdorner/roboreader/persistence/DatabaseHelper.java
@@ -9,12 +9,15 @@ import android.util.Log;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+import com.j256.ormlite.dao.Dao;
 
 public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
   protected final String TAG = this.getClass().getName();
 
   private static final String DATABASE_NAME = "roboreader.db";
   private static final int DATABASE_VERSION = 1;
+
+  private Dao<Book, Integer> bookDao = null;
 
   public DatabaseHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -31,6 +34,13 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
 
   public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
     Log.d(TAG, "onUpgrade");
+  }
+
+  public Dao<Book, Integer> getBookDao() throws SQLException {
+    if(bookDao == null) {
+      bookDao = getDao(Book.class);
+    }
+    return bookDao;
   }
 }
 
