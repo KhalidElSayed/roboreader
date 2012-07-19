09f1d37e9c9b9ac2a1eeeb26359b424a047138b2
Christof Dorner <christof@chdorner.me>
2012-07-19 22:40:04 +0200

    Allow importing epubs via Intents

--- src/com/chdorner/roboreader/LibraryActivity.java
+++ src/com/chdorner/roboreader/LibraryActivity.java
@@ -1,15 +1,42 @@
 package com.chdorner.roboreader;
 
+import com.chdorner.roboreader.tasks.ImportEPUBTask;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+import android.net.Uri;
+import android.widget.Toast;
 
-public class LibraryActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class LibraryActivity extends Activity {
+  private final String TAG = getClass().getName();
+
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    Intent intent = getIntent();
+    if("application/epub+zip".equals(getIntent().getType())) {
+      importBook(this, intent);
     }
+  }
+
+  private void importBook(Context context, Intent intent) {
+    if(!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+      Toast.makeText(context, "Cannot import Book at the moment", Toast.LENGTH_LONG).show();
+      return;
+    }
+
+    Toast.makeText(context, "Importing book...", Toast.LENGTH_SHORT).show();
+    new ImportEPUBTask(this).execute(intent.getData());
+  }
 }
