1120178e7e0682b3a7f4977123d135d928be97d6
Christof Dorner <christof@chdorner.me>
2012-07-19 22:39:11 +0200

    Add util class for generating MD5 hashes

---
+++ src/com/chdorner/roboreader/util/MD5.java
@@ -0,0 +1,29 @@
+package com.chdorner.roboreader.util;
+
+import android.util.Log;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5 {
+  private static final String TAG = "com.chdorner.roboreader.util.MD5";
+
+  public static String hexdigest(String str) {
+    try {
+      MessageDigest digest = MessageDigest.getInstance("MD5");
+      digest.update(str.getBytes());
+      byte messageDigest[] = digest.digest();
+
+      StringBuffer hexString = new StringBuffer();
+      for (int i=0; i<messageDigest.length; i++) {
+        hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+      }
+
+      return hexString.toString();
+    } catch (NoSuchAlgorithmException e) {
+      Log.e(TAG, e.getMessage());
+    }
+    
+    return "";
+  }
+}
+
